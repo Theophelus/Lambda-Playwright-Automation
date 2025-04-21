b846442e5eb71029c438262c6a5dc440cf95d62c
Theophelus <aneletom10@gmail.com>
2025-04-21 14:18:40 +0200

    import dependencies and get logging dir, where all logs will be stored

---
+++ src/utils/LoggerUtils.ts
@@ -0,0 +1,8 @@
+import winston from "winston";
+import moment from "moment-timezone";
+import chalk from "chalk";
+import path from "path";
+
+//navigate/find to logging dir
+const logginDir = path.resolve(__dirname, "..", "logging");
+
