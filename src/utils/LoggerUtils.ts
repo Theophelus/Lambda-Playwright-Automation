15b0113b34c513cd901bf81d48831f9c5a6e8d26
Theophelus <aneletom10@gmail.com>
2025-04-21 14:25:26 +0200

    Implement custom format

--- src/utils/LoggerUtils.ts
+++ src/utils/LoggerUtils.ts
@@ -6,3 +6,17 @@ import path from "path";
 //navigate/find to logging dir
 const logginDir = path.resolve(__dirname, "..", "logging");
 
+//format log entries
+const customFormat = winston.format.printf(({ level, message, timestamp }) => {
+  let chalkColorPalletteLevel = level;
+
+  switch (level) {
+    case "info":
+      chalkColorPalletteLevel = chalk.green(level);
+      break;
+    case "error":
+      chalkColorPalletteLevel = chalk.red(level);
+      break;
+  }
+  return `${timestamp} [${chalkColorPalletteLevel}]: ${message}`;
+});
