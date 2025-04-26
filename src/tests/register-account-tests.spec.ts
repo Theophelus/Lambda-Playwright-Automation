ba8ceadfeb0e00cba4c74aa382e160520f166357
Theophelus <aneletom10@gmail.com>
2025-04-26 17:08:36 +0200

    Adding a test were user should be able to register

--- src/tests/register-account-tests.spec.ts
+++ src/tests/register-account-tests.spec.ts
@@ -1,7 +1,25 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import RegistrationAccountPage from "../pages/RegistrationAccountPage";
-
 
 const registrationAccountData = require("../data/registrationAccountData.json");
 
+test("Users should be able to register to Lambda Play pen", async ({
+  page,
+}) => {
+  const registrationAccountPage = new RegistrationAccountPage(page);
 
+  // Initiator
+  await page.goto("/", { waitUntil: "domcontentloaded" });
+  await registrationAccountPage.hoverMyAccount();
+  await registrationAccountPage.clickMyAccountLinks();
+
+  //verify if landed on the correct page
+  await expect(page).toHaveTitle("Register Account");
+
+  // Fill the form
+  await registrationAccountPage.fillInRegistrationForm(registrationAccountData);
+  await registrationAccountPage.clickContinueBtn();
+  
+  //assertions
+  await registrationAccountPage.verifySuccessMessage();
+});
