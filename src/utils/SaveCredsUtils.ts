d076e39c03a04dcf6917924fcb29e7efb670f409
Theophelus <aneletom10@gmail.com>
2025-04-26 16:17:44 +0200

    adding a utility to save and store creds

---
+++ src/utils/SaveCredsUtils.ts
@@ -0,0 +1,13 @@
+/**
+ * In a scenarion where this utility runs, would be in your pre-requisites, stage of your pipeline to store new user creds and reuse the across demanding tests.
+ */
+
+/**import modules */
+import fs from "fs";
+import path from "path";
+
+function saveCreds(map: Map<string, string>): void {
+ 
+}
+
+export default saveCreds;
