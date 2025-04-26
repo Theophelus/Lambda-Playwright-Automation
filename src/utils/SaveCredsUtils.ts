debe12cd544380bae4e3e5326dd991aeea95a48c
Theophelus <aneletom10@gmail.com>
2025-04-26 16:50:57 +0200

    update values and update .env file

--- src/utils/SaveCredsUtils.ts
+++ src/utils/SaveCredsUtils.ts
@@ -13,13 +13,30 @@ function saveCreds(map: Map<string, string>): void {
     const file_path = path.resolve(__dirname, "..", "config/.env");
     //read and spit the .env into lines
     const readEnvFile = fs.readFileSync(file_path, "utf8").split("\n");
-    
+
+    let updateEnvValues = readEnvFile.map((line) => {
+      //split and get each key
+      const [key] = line.split("=");
+      logger.info(`.envi key values: ${key}`);
+      //update the value if the key exists in the mpa
+      if (map.has(key.trim())) {
+        return `${key.trim()}=${map.get(key.trim())}`;
+      }
+
+      //else keep the map unchanged
+      return line;
+    });
+
+    //write updated values unti the .env file
+    fs.writeFileSync(file_path, updateEnvValues.join("\n"), {
+      encoding: "utf8",
+    });
+
     logger.info(readEnvFile);
   } catch (error) {
-    logger.error(`Error while updating the .env file: ${error.message}`);
+    logger.error(`Error while updating the .env file: ${error}`);
+    throw error;
   }
-
-
 }
 
 export default saveCreds;
