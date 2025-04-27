7e3d2831336121211a9863c2ac8d8437b2c66d56
Theophelus <aneletom10@gmail.com>
2025-04-27 15:08:35 +0200

    controlling user reg state, if already created skip the test

--- src/tests/register-account-tests.spec.ts
+++ src/tests/register-account-tests.spec.ts
@@ -1,13 +1,20 @@
 import { test, expect } from "@playwright/test";
 import RegistrationAccountPage from "../pages/RegistrationAccountPage";
+import logger from "../utils/LoggerUtils";
 
 const registrationAccountData = require("../data/registrationAccountData.json");
 
-test("Users should be able to register to Lambda Play pen", async ({
-  page,
-}) => {
-  const registrationAccountPage = new RegistrationAccountPage(page);
+test("Users should be able to register to Lambda Play pen", async ({ page }) => {
+  //check reg state is created already
+  let registration_already_ran: boolean;
+  registration_already_ran = process.env.registration_test_completed === "true";
 
+  if (registration_already_ran) {
+    logger.info("✅ Registration for this user already completed..!");
+    test.skip(true, "Registration already completed.");
+  }
+
+  const registrationAccountPage = new RegistrationAccountPage(page);
   // Initiator
   await page.goto("/", { waitUntil: "domcontentloaded" });
   await registrationAccountPage.hoverMyAccount();
