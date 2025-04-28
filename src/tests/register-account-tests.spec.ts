6dd8969529548f6c9aca9de6ac6d9b2d9794bf95
Theophelus <aneletom10@gmail.com>
2025-04-28 15:40:46 +0200

    updating .env file when registaring an account. This will ensure the test runs once

--- src/tests/register-account-tests.spec.ts
+++ src/tests/register-account-tests.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from "../fixtures/PageFixture";
 import logger from "../utils/LoggerUtils";
+import saveCreds from "../utils/SaveCredsUtils";
 
 const registrationAccountData = require("../data/registrationAccountData.json");
 
-test("Users should be able to register to Lambda Play pen", async ({homePage,
+test("Users should be able to register to Lambda Play pen", async ({
+  homePage,
   registrationAccountPage,
 }) => {
   //check reg state is created already
-  let registration_already_ran: boolean;
+  let registration_already_ran: any;
   registration_already_ran = process.env.registration_test_completed === "true";
 
   if (registration_already_ran) {
@@ -32,4 +34,15 @@ test("Users should be able to register to Lambda Play pen", async ({homePage,
 
   //assertions
   await registrationAccountPage.verifySuccessMessage();
+
+  /**
+   * if registration state is false on .env file then this code will run to update to true
+   */
+  const registration_state: Map<string, string> = new Map<string, string>();
+  registration_already_ran = "true";
+  registration_state.set(
+    "registration_test_completed",
+    registration_already_ran
+  );
+  saveCreds(registration_state);
 });
