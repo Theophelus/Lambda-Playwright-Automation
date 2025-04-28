ab960db29b98da755758c73e78fbd6ac11d4453d
Theophelus <aneletom10@gmail.com>
2025-04-28 17:14:19 +0200

    Moving hoverMyAccount method to HeaderComponents class for code reuse

--- src/components/HeaderComponents.ts
+++ src/components/HeaderComponents.ts
@@ -12,4 +12,16 @@ export class HeaderComponents {
     );
   }
 
+  /**hover over My account dropdown menu */
+  async hoverMyAccount(): Promise<void> {
+    try {
+      await this.hoverMyAccountDropdownMenuSelector.hover();
+      logger.info("✅ successfully hovered over 'My account' dropdown menu.");
+    } catch (error) {
+      logger.error(
+        `❌ Error while hovering over My account dropdown menu: ${error}`
+      );
+      throw error;
+    }
+  }
 }
