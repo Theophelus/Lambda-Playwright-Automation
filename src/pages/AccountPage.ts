e26b0730aff77fb0f3c7d8c20f57752a349de7dc
Theophelus <aneletom10@gmail.com>
2025-05-10 14:50:27 +0200

    Reusing hoverMyAccountMethod on HomePage

--- src/pages/AccountPage.ts
+++ src/pages/AccountPage.ts
@@ -1,11 +1,13 @@
 import { test, Page, expect, Locator } from "@playwright/test";
+import { HeaderComponents } from "../components/HeaderComponents";
 
 export class AccountPage {
-
   private readonly myAccountHeaderLocator: Locator;
-  
+  private readonly headerComponent: HeaderComponents;
+
   constructor(private page: Page) {
     this.page = page;
+    this.headerComponent = new HeaderComponents(page);
     this.myAccountHeaderLocator = this.page.locator(
       "//h2[contains(text(), 'My Account')]"
     );
@@ -13,5 +15,9 @@ export class AccountPage {
 
   async expectedMyAccountTitleToBeVisible(): Promise<void> {
     await expect(this.myAccountHeaderLocator).toBeVisible({ timeout: 5000 });
+  }
+
+  async hoverOverMyAccount(): Promise<void> {
+    await this.headerComponent.hoverMyAccount();
   }
 }
