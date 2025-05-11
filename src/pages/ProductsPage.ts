e53e8a4935ece007ee58447ea2311c3ad7d2e37e
Theophelus <aneletom10@gmail.com>
2025-05-11 14:10:04 +0200

    Adding a method to search for a specific product and click that product

--- src/pages/ProductsPage.ts
+++ src/pages/ProductsPage.ts
@@ -17,4 +17,39 @@ export class ProductsPage {
       "//div//ul[@class='dropdown-menu autocomplete w-100']//li//h4//a"
     );
   }
+
+  /**
+   * @method to filter a specif product from the 'Search For Product' textbox and click it
+   * @param product to control the filter base
+   */
+
+  async searchForASpecificProductAndClick(product: string): Promise<void> {
+    //search for a product
+    await typeRequireText(product, this.searchForProductSelector);
+
+    //implicit wait to allow elements to load
+    await this.page.waitForTimeout(4000);
+
+    //filter for specific product by text
+    const filtered_product = this.listOfSearchProductSelector.filter({
+      hasText: product,
+    });
+
+    //assert list is not empty
+    expect(await filtered_product.count()).toBeGreaterThan(0);
+
+    //check if element exists and click
+    try {
+      if ((await filtered_product.count()) > 0) {
+        //click the first product that meets the criteria
+        await filtered_product.first().click();
+        //implicit wait to allow elements to load
+        await this.page.waitForTimeout(20000);
+        logger.info(`✅ clicking ${product} product from 'Search For Product' textbox.`);
+      }
+    } catch (error) {
+      logger.error(`❌ Error while trying to click '${product}': ${error}`);
+      throw error;
+    }
+  }
 }
