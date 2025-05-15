adfc9c0a4260178dc7dd8a5afc48b96e3f5e9933
Theophelus <aneletom10@gmail.com>
2025-05-15 17:38:25 +0200

    Adding a method to filter for specific products from the list and add into the cart

--- src/pages/ProductsPage.ts
+++ src/pages/ProductsPage.ts
@@ -3,6 +3,7 @@ import { typeRequireText } from "../components/Inputs";
 import logger from "../utils/LoggerUtils";
 import { HeaderComponents } from "../components/HeaderComponents";
 import { clickSpecificCategory } from "../components/CategoryComponent";
+import { add } from "winston";
 
 export class ProductsPage {
   //define locators
@@ -11,6 +12,7 @@ export class ProductsPage {
   private readonly productHeaderSelector: Locator;
   private readonly topCategorySelector: Locator;
   private readonly categoryTitleSelector: Locator;
+  private readonly listOfProductsSelector: Locator;
   private readonly headerComponent: HeaderComponents;
 
   constructor(private page: Page) {
@@ -28,6 +30,9 @@ export class ProductsPage {
       "//ul[@class='navbar-nav vertical']//li//a"
     );
     this.categoryTitleSelector = this.page.locator("#entry_212392 h1");
+    this.listOfProductsSelector = this.page.locator(
+      "//*[@class='product-layout product-grid no-desc col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6']//div[@class='product-thumb']"
+    );
   }
 
   /**
@@ -107,6 +112,48 @@ export class ProductsPage {
     } catch (error) {
       logger.error(`❌ Category title could not be verified: ${error}`);
       throw error;
+    }
+  }
+  /**
+   * @method, define a method that takes one @parameter to add a product to the cart.
+   */
+  async addProductToCart(productName: string): Promise<void> {
+    //define local selectors
+    const next_button_selector = this.page.locator(
+      '//*[@id="entry_212409"]/div/div[1]/ul/li[6]/a'
+    );
+
+    //define a variable to control the loop
+    let is_found: boolean = false;
+
+    //define a while loop as long the value is not false
+    while (!is_found) {
+      let list_of_products = await this.listOfProductsSelector.all();
+      //filter the list and hover over a product before selecting
+      for (let current_product of list_of_products) {
+        if ((await current_product.innerText()).includes(productName)) {
+          is_found = true;
+          await current_product.hover();
+
+          //click cart icon
+          const add_to_cart_btn = current_product
+            .locator("..//button[@title='Add to Cart']")
+            .first();
+          await add_to_cart_btn.hover({ timeout: 4000 });
+          await add_to_cart_btn.dblclick();
+          break;
+        }
+      }
+
+      //break if the product is found
+      if (is_found) break;
+
+      //check if the product is not found then go th the next page
+      if ((await next_button_selector.count()) > 0) {
+        await next_button_selector.click();
+        //refresh product list
+        list_of_products = await this.listOfProductsSelector.all();
+      }
     }
   }
 }
