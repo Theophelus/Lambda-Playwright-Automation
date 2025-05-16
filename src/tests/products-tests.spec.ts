4b64b9544d5dd666c3db584c56659f47e6df92a5
Theophelus <aneletom10@gmail.com>
2025-05-16 19:23:11 +0200

    Adding a test case to search and add a product to the cart

--- src/tests/products-tests.spec.ts
+++ src/tests/products-tests.spec.ts
@@ -44,13 +44,45 @@ test.describe("Products pages test cases", () => {
     const accountPage = await loginPage.clickLoginBtn();
     //assertion
     await accountPage.expectedMyAccountTitleToBeVisible();
-
     //click Shop by Category Link
     await productsPage.clickShopByCategoryLink();
     //select category from the list
     await productsPage.selectSpecificCategory(" Laptops & Notebooks");
-
     //Assert title
     await productsPage.verifyCategoryTitle();
+
+  });
+
+  test("Verify that a Returning Customer can Search and Add a Product to the Cart.", async ({
+    homePage,
+    loginPage,
+    productsPage,
+  }) => {
+    await homePage.navigate();
+
+    //action
+    await loginPage.hoverMyAccount();
+    await loginPage.clickLoginAccountLink();
+    //fill form
+    await loginPage.fillInLoginForm();
+    //click login button
+    const accountPage = await loginPage.clickLoginBtn();
+    //assertion
+    await accountPage.expectedMyAccountTitleToBeVisible();
+    //click Shop by Category Link
+    await productsPage.clickShopByCategoryLink();
+    //select category from the list
+    await productsPage.selectSpecificCategory(" Laptops & Notebooks");
+    //Assert title
+    await productsPage.verifyCategoryTitle();
+
+    //filter for a specific product
+    let searched_product = "iPod Touch";
+    const cart_page = await productsPage.addProductToCart(searched_product);
+
+    //click cart page and verify product is added successfully to the cart
+    await cart_page.clickViewCartIcon();
+    // await cart_page
+    await cart_page.verifyProductNameInTheCart(searched_product);
   });
 });
