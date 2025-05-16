98390cef575238a57364306d47ec15fa04074c43
Theophelus <aneletom10@gmail.com>
2025-05-16 18:56:47 +0200

    Adding a method to click view cart button

--- src/pages/CartPage.ts
+++ src/pages/CartPage.ts
@@ -1,5 +1,30 @@
-import { Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
+import logger from "../utils/LoggerUtils";
 
 export class CartPage {
-  constructor(private page: Page) {}
+  private readonly viewCartSelector: Locator;
+  private tableSelector: Locator;
+
+  constructor(private page: Page) {
+    this.page = page;
+    this.viewCartSelector = this.page.locator(
+      "//a[@class='btn btn-primary btn-block']"
+    );
+    this.tableSelector = this.page.locator(
+      "//table[@class='table table-bordered']//tbody"
+    );
+  }
+
+  /**
+   * @method to click view cart button
+   */
+  async clickViewCartIcon(): Promise<void> {
+    try {
+      await this.viewCartSelector.click();
+      logger.info(`✅ Clicked 'View Cart' button`);
+    } catch (error) {
+      logger.error(`❌ Could not click 'View Cart' button: ${error}`);
+      throw error;
+    }
+  }
 }
