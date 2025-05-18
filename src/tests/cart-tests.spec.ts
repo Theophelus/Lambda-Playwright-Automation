0d7663b5ed3a1850cc7cbbbe4196899e696e1f71
Theophelus <aneletom10@gmail.com>
2025-05-18 17:21:56 +0200

    Improving readability of 'Returning Customer should be able to update product quantity' test case

--- src/tests/cart-tests.spec.ts
+++ src/tests/cart-tests.spec.ts
@@ -27,11 +27,12 @@ test.describe(`Cart Page - Registered Customer`, () => {
   }) => {
     const product_data = {
       product_name: "iPod Nano",
-      product_quantity: 4
+      product_quantity: 4,
       // product_total: "$186.40",
     };
     //test execution
     await homePage.navigate();
+    //login into Lambda Playground
     await loginPage.hoverMyAccount();
     await loginPage.clickLoginAccountLink();
     await loginPage.fillInLoginForm();
@@ -39,12 +40,18 @@ test.describe(`Cart Page - Registered Customer`, () => {
     const account_page = await loginPage.clickLoginBtn();
     await account_page.expectedMyAccountTitleToBeVisible();
 
+    //filter and add product to the cart
     await productsPage.clickShopByCategoryLink();
     await productsPage.selectSpecificCategory("Laptops & Notebooks");
     await productsPage.addProductToCart(product_data.product_name);
+    //Verify the cart
     await cartPage.clickViewCartIcon();
+    await cartPage.verifyProductNameInTheCart(product_data.product_name);
     //update cart quantity per product to
-    await cartPage.updateProductQuantity(product_data.product_name, product_data.product_quantity);
+    await cartPage.updateProductQuantity(
+      product_data.product_name,
+      product_data.product_quantity
+    );
     // await cartPage.verifyProductTotalPriceIsRecalculated();
   });
 });
