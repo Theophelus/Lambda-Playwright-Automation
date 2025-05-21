2ff4521d1b07032fe4daea4d0da8c79a00710282
Theophelus <aneletom10@gmail.com>
2025-05-21 12:03:18 +0200

    updating verifyProductNameInTheCart method incase we have more than 1 product in the cart

--- src/pages/CartPage.ts
+++ src/pages/CartPage.ts
@@ -38,27 +38,27 @@ export class CartPage {
    */
   async verifyProductNameInTheCart(productName: string): Promise<void> {
     await this.page.waitForLoadState("networkidle");
+    let is_found = false;
+    let index = 0;
+    while (!is_found) {
+      try {
+        //get productName cell in the first row
+        const filter_prod_name = await this.filterEachCell(this.tableSelector, index, 1);
+        //check if product_name is found and assert
+        if ((await filter_prod_name.innerText()).includes(productName)) {
+          await expect(await filter_prod_name.innerText()).toBe(productName);
+          logger.info(`✅ ${productName} is added to the cart successfully verified`);
+          is_found = true;
+          break;
+        }
+        index++;
 
-    try {
-      //get productName cell in the first row
-      const filter_prod_name = await this.filterEachCell(
-        this.tableSelector,
-        0,
-        1
-      );
-
-      //check if product_name is found and assert
-      if ((await filter_prod_name.innerText()).includes(productName)) {
-        await expect(await filter_prod_name.innerText()).toBe(productName);
-        logger.info(
-          `✅ ${productName} is added to the cart successfully verified`
-        );
+        if(is_found) break;
+        //not found
+        throw new Error(`${productName} is not found`);
+      } catch (error) {logger.error(`❌ ${productName} not found and verified in the cart: ${error}`);
+        throw error;
       }
-    } catch (error) {
-      logger.error(
-        `❌ ${productName} not found and verified in the cart: ${error}`
-      );
-      throw error;
     }
   }
 
@@ -85,7 +85,7 @@ export class CartPage {
       logger.info(`${message} is displayed and verified.`);
       expect(await this.cartMessageSelector.innerText()).toBe(message);
     } catch (error) {
-      logger.error(`${message} is not displayed: ${error}`);
+      logger.error(`❌ ${message} is not displayed: ${error}`);
       throw error;
     }
   }
@@ -105,7 +105,7 @@ export class CartPage {
 
     //check any matching row are found
     if ((await returned_selector.count()) === 0) {
-      throw new Error(`No row found for provided index: ${index}`);
+      throw new Error(`❌ No row found for provided index: ${index}`);
     }
 
     //return first matching row
@@ -137,12 +137,20 @@ export class CartPage {
     //go through each row
     while (!is_found) {
       try {
-        let current_cell = await this.filterEachCell(this.tableSelector,index,1);
+        let current_cell = await this.filterEachCell(
+          this.tableSelector,
+          index,
+          1
+        );
         let product_cell_name = await current_cell.innerText();
         //check if product_cell_name meet condition
         if (product_cell_name.includes(product_name)) {
           // get product rows
-          const product_quantity_cell: Locator = await this.filterEachCell(this.tableSelector,index,3);
+          const product_quantity_cell: Locator = await this.filterEachCell(
+            this.tableSelector,
+            index,
+            3
+          );
           let update_quantity = product_quantity_cell.locator("//..//input");
           let press_update_btn = product_quantity_cell.locator(
             "//..//button[@type='submit']"
@@ -154,7 +162,7 @@ export class CartPage {
           // press update quantity button
           await press_update_btn.click();
           logger.info(
-            `${product_name} quantity have been updated to ${product_quantity}`
+            `✅ ${product_name} quantity have been updated to ${product_quantity}`
           );
           is_found = true;
           return true;
@@ -162,7 +170,7 @@ export class CartPage {
         index++;
       } catch (error) {
         logger.error(
-          `$Error occured while updating product: ${product_name} to quantity of: ${product_quantity}: ${error}`
+          `❌ $Error occured while updating product: ${product_name} to quantity of: ${product_quantity}: ${error}`
         );
         throw error;
       }
@@ -187,41 +195,56 @@ export class CartPage {
         //check current product meets criteria
         if ((await (await curreny_cell).innerText()).includes(product)) {
           //get current product quantity value
-          let product_quantity = this.filterEachCell( this.tableSelector, index, 3);
-          let product_quantity_value: any =  (await product_quantity).locator("//..//input").getAttribute("value");
+          let product_quantity = this.filterEachCell(
+            this.tableSelector,
+            index,
+            3
+          );
+          let product_quantity_value: any = (await product_quantity)
+            .locator("//..//input")
+            .getAttribute("value");
 
           //get the unit price of the current_cell
-          let get_unit_price = (await this.filterEachCell(this.tableSelector, index, 4)).innerText();
-          let unit_price: any = parseFloat((await get_unit_price).split("$")[1]);
+          let get_unit_price = (
+            await this.filterEachCell(this.tableSelector, index, 4)
+          ).innerText();
+          let unit_price: any = parseFloat(
+            (await get_unit_price).split("$")[1]
+          );
 
-          let total_price = parseFloat(await product_quantity_value || "0") * unit_price;
-          let product_total_price = (await (await this.filterEachCell(this.tableSelector, index, 5)).innerText());
+          let total_price =
+            parseFloat((await product_quantity_value) || "0") * unit_price;
+          let product_total_price = await (
+            await this.filterEachCell(this.tableSelector, index, 5)
+          ).innerText();
 
-    
-          const expected_price_total = total_price.toLocaleString('en-US',
-            {
-              style:'currency',
-              currency: 'USD',
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2
-            })
-          
-            //assert 
+          const expected_price_total = total_price.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          });
+
+          //assert
           expect(expected_price_total).toBe(product_total_price);
-          logger.info(`Product total price of: ${product_total_price} have been verified, based on product Quantity updated: ${await product_quantity_value} and Unit Price of : ${await get_unit_price}`)
+          logger.info(
+            `✅ Product total price of: ${product_total_price} have been verified, based on product Quantity updated: ${await product_quantity_value} and Unit Price of : ${await get_unit_price}`
+          );
           is_found = true;
           break;
         }
 
         index++;
       } catch (error) {
-        logger.error(`$Error occured while updating product: to quantity of: ${error}`);
+        logger.error(
+          `❌ $Error occured while updating product: to quantity of: ${error}`
+        );
         throw error;
       }
       //break the loop once condition met
       if (is_found) break;
       //if product not found
-      throw new Error(`${product} not found in the Cart`);
+      throw new Error(`❌ ${product} not found in the Cart`);
     }
   }
 }
