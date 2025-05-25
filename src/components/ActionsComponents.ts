982339b5d51b0fd4740949c555f8dcadc7b7f1e0
Theophelus <aneletom10@gmail.com>
2025-05-25 16:33:34 +0200

    Adding a action component to handle playwright actions

---
+++ src/components/ActionsComponents.ts
@@ -0,0 +1,19 @@
+import { Locator, Page } from "@playwright/test";
+
+export class ActionsComponents {
+  constructor(private page: Page) {}
+
+  //click elements in the dom
+  async click(locator: Locator, element?: string): Promise<void> {
+    //get element innerText();
+    await locator.click();
+  }
+  //fill input box
+  async fill(locator: Locator,value: string,element_description?: string): Promise<void> {
+    await locator.fill(value);
+  }
+  //check checkboxes and radio button
+  async check(locator: Locator): Promise<void>{
+    await locator.check();
+  }
+}
