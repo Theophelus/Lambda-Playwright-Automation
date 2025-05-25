ae89e95775f90e279daa0e7cf74e8920f6783e74
Theophelus <aneletom10@gmail.com>
2025-05-25 15:46:08 +0200

    Implementing HelperComponent for code reuse

--- src/pages/AccountPage.ts
+++ src/pages/AccountPage.ts
@@ -1,20 +1,36 @@
 import { test, Page, expect, Locator } from "@playwright/test";
 import { HeaderComponents } from "../components/HeaderComponents";
+import { PageInterface } from "./PageInterface";
+import { AssertionsComponents } from "../components/AssertionsComponents";
+import { HelperComponents } from "../components/HelperComponents";
 
 export class AccountPage {
-  private readonly myAccountHeaderLocator: Locator;
+  // private readonly page: Page;
   private readonly headerComponent: HeaderComponents;
+  // private readonly components: PageInterface;
+  private readonly assertion: AssertionsComponents;
+  private readonly helper: HelperComponents;
+
+  //locators
+  private readonly myAccountHeader: Locator;
 
   constructor(private page: Page) {
     this.page = page;
-    this.headerComponent = new HeaderComponents(page);
-    this.myAccountHeaderLocator = this.page.locator(
+    //initialize components
+    this.headerComponent = new HeaderComponents(this.page);
+    this.assertion = new AssertionsComponents(this.page);
+    this.helper = new HelperComponents(this.page);
+    //initialize locators
+    this.myAccountHeader = this.page.locator(
       "//h2[contains(text(), 'My Account')]"
     );
   }
 
   async expectedMyAccountTitleToBeVisible(): Promise<void> {
-    await expect(this.myAccountHeaderLocator).toBeVisible({ timeout: 5000 });
+    //hightlight element
+    await this.helper.elementHighlighter(this.myAccountHeader);
+    //verify element
+    await this.assertion.assertVesible(this.myAccountHeader);
   }
 
   async hoverOverMyAccount(): Promise<void> {
