8d8c9ade96c13b33fe6ebe1e9717c5b08037f19e
Theophelus <aneletom10@gmail.com>
2025-05-31 16:47:34 +0200

    Adding navigation component

--- src/pages/PageComponents.ts
+++ src/pages/PageComponents.ts
@@ -2,15 +2,17 @@ import { AssertionsComponents } from "../components/AssertionsComponents";
 import { HeaderComponents } from "../components/HeaderComponents";
 import { HelperComponents } from "../components/HelperComponents";
 import * as CategoryComponent from "../components/CategoryComponent";
-import * as Inputs from "../components/Inputs"
+import * as Inputs from "../components/Inputs";
 import { ActionsComponents } from "../components/ActionsComponents";
+import { NavigationComponent } from "../components/NavigationComponent";
 
 //group all components for easy use across pom's
-export interface PageComponents{
-    actions?: ActionsComponents
-    assertions?: AssertionsComponents;
-    helper?: HelperComponents,
-    header?: HeaderComponents,
-    category?: typeof CategoryComponent,
-    input?: typeof Inputs
+export interface PageComponents {
+  actions?: ActionsComponents;
+  assertions?: AssertionsComponents;
+  helper?: HelperComponents;
+  header?: HeaderComponents;
+  navigation?: NavigationComponent;
+  category?: typeof CategoryComponent;
+  input?: typeof Inputs;
 }
